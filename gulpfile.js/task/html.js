7c1971f0e1af2eddb019b9f964c229beaaf4f806
adisarov <adisarov.dev@gmail.com>
2022-01-23 11:52:37 +0200

    Update html.js

--- gulpfile.js/task/html.js
+++ gulpfile.js/task/html.js
@@ -1,14 +1,16 @@
 const { src, dest } = require("gulp");
 
-const fileInclude = require("gulp-file-include");
-const htmlmin = require("gulp-htmlmin");
-
 const path = require("../config/path.js");
 const app = require("../config/app.js");
+
+const fileInclude = require("gulp-file-include");
+const typograf = require("gulp-typograf");
+const htmlmin = require("gulp-htmlmin");
 
 const html = () => {
   return src(path.html.src)
     .pipe(fileInclude(app.fileInclude))
+    .pipe(typograf(app.typograf))
     .pipe(htmlmin(app.htmlmin))
     .pipe(dest(path.html.dest))
 }
